aa8c0197c1bb5d09edfb23b0d7f8f9e06d7b7eb1
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-08-10 18:38:41 +0300

    Add Auth Test Controller

--- MangoFusion_API/Models/MenuItem.cs
+++ MangoFusion_API/Models/MenuItem.cs
@@ -10,7 +10,7 @@ namespace MangoFusion_API.Models
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; } 
         public string Category { get; set; } = string.Empty;
-        public string SpecialTag { get; set; } 
+        public string? SpecialTag { get; set; } 
         [Range(1, 1000)]
         public double Price { get; set; }
         [Required]
