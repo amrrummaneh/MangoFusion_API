c6e7d726547ea4620c52d9f06ecbff3199c11c8f
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-08-10 11:29:47 +0300

    Create Menu Item

--- MangoFusion_API/Models/DTO/MenuItemCreateDTO.cs
+++ MangoFusion_API/Models/DTO/MenuItemCreateDTO.cs
@@ -1,15 +1,15 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System.ComponentModel.DataAnnotations;
 
-namespace MangoFusion_API.Models.DTO
+namespace MangoFusion_API.Models.Dto
 {
     public class MenuItemCreateDTO
     {
         [Required]
         public string Name { get; set; } = string.Empty;
-        public string? Description { get; set; }
+        public string Description { get; set; } = string.Empty;
         public string Category { get; set; } = string.Empty;
-        public string? SpecialTag { get; set; }
+        public string SpecialTag { get; set; } = string.Empty;
         [Range(1, 1000)]
         public double Price { get; set; }
         public IFormFile File { get; set; } = null!;
--- MangoFusion_API/Models/DTO/MenuItemUpdateDTO.cs
+++ MangoFusion_API/Models/DTO/MenuItemUpdateDTO.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace MangoFusion_API.Models.DTO
+namespace MangoFusion_API.Models.Dto
 {
     public class MenuItemUpdateDTO
     {
@@ -8,11 +8,11 @@ namespace MangoFusion_API.Models.DTO
         public int Id { get; set; }
         [Required]
         public string Name { get; set; } = string.Empty;
-        public string? Description { get; set; }
+        public string Description { get; set; } = string.Empty;
         public string Category { get; set; } = string.Empty;
-        public string? SpecialTag { get; set; }
+        public string SpecialTag { get; set; } = string.Empty;
         [Range(1, 1000)]
         public double Price { get; set; }
-        public IFormFile? File { get; set; } 
+        public IFormFile File { get; set; } = null!;
     }
 }
