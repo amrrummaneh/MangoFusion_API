299426e906877ba5cccf33171879a989acca1db0
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-08-10 11:10:34 +0300

    DTOs for Create and Update Menu Item

--- MangoFusion_API/Models/DTO/MenuItemUpdateDTO.cs
+++ MangoFusion_API/Models/DTO/MenuItemUpdateDTO.cs
@@ -1,19 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace MangoFusion_API.Models
+namespace MangoFusion_API.Models.DTO
 {
-    public class MenuItem
+    public class MenuItemUpdateDTO
     {
         [Key]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; } = string.Empty;
-        public string? Description { get; set; } 
+        public string? Description { get; set; }
         public string Category { get; set; } = string.Empty;
-        public string SpecialTag { get; set; } 
+        public string? SpecialTag { get; set; }
         [Range(1, 1000)]
         public double Price { get; set; }
-        [Required]
-        public string Image { get; set; } = string.Empty;
+        public IFormFile? File { get; set; } 
     }
 }
