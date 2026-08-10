ff9b397084d8b9b0494521b1d01ef94e2e0800f5
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-08-10 09:48:24 +0300

    API Response

---
+++ MangoFusion_API/Models/API Response.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace MangoFusion_API.Models
+{
+    public class API_Response
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public List<string> ErrorMessages { get; set; } = [];
+        public object? Result { get; set; }
+    }
+}
