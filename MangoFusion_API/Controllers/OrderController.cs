3afffa65c02fd1e5af10715a12bb574c4a290273
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-08-13 10:41:24 +0300

    Demo - Update Order

--- MangoFusion_API/Controllers/OrderController.cs
+++ MangoFusion_API/Controllers/OrderController.cs
@@ -129,6 +129,13 @@ namespace MangoFusion_API.Controllers
             {
                 if (ModelState.IsValid)
                 {
+                    if (orderId != orderHeaderDTO.OrderHeaderId)
+                    {
+                        _response.IsSuccess = false;
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        _response.ErrorMessages.Add("Invalid Id");
+                        return BadRequest(_response);
+                    }
                     OrderHeader? orderHeaderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.OrderHeaderId == orderId);
 
                     if (orderHeaderFromDb == null)
